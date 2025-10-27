305f421dd6f05975fea099c4959172666b6da5fc
liyuxuan <liyuxuan3003@outlook.com>
2025-10-27 09:04:45 +0800

    重新调整default值设置方式

--- MagicLeda.scala
+++ MagicLeda.scala
@@ -4,11 +4,13 @@ import spinal.core._
 import spinal.lib._
 
 case class MagicLeda() extends Bundle with IMasterSlave {
-  val clk = Bool().default(Bool())
-  val reset = Bool().default(Bool())
-  val key = Bits(5 bits).default(0)
-  val switch = Vec(Bits(8 bits), 2).default(Vec(B(0, 8 bits), 2))
-  val led = Vec(Bits(8 bits), 2).default(Vec(B(0, 8 bits), 2))
+  val clk = Bool()
+  val reset = Bool()
+  val key = Bits(5 bits)
+  val switch = Vec(Bits(8 bits), 2)
+  val led = Vec(Bits(8 bits), 2)
+
+  led.default(Vec(B(0, 8 bits), 2))
 
   override def asMaster(): Unit = {
     in(clk, reset, key, switch)
