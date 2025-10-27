b91071765ebefd98ab6f18a9853d5588d328eafc
liyuxuan <liyuxuan3003@outlook.com>
2025-10-27 08:35:34 +0800

    增加配套的flash型号

--- magicleda.mk
+++ magicleda.mk
@@ -7,3 +7,4 @@ CURRENT_DIR?=$(patsubst %/,%,$(dir $(lastword ${MAKEFILE_LIST})))
 # MagicLeda
 LEDA_XDC?=${CURRENT_DIR}/MagicLeda.xdc
 LEDA_PLATFORM?=xc7a35tcsg324-1
+LEDA_FLASH?=n25q64-3.3v-spi-x1_x2_x4
