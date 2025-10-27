2ea33c40fd403481424645ca948ec10bb09ab55b
liyuxuan <liyuxuan3003@outlook.com>
2025-10-27 09:04:26 +0800

    调整了makefile内的命名，标明了自己的scala代码

--- magicleda.mk
+++ magicleda.mk
@@ -2,9 +2,10 @@
 
 # --------------------------------
 # Get current dir
-CURRENT_DIR?=$(patsubst %/,%,$(dir $(lastword ${MAKEFILE_LIST})))
+THIS_DIR?=$(patsubst %/,%,$(dir $(lastword ${MAKEFILE_LIST})))
 
 # MagicLeda
-LEDA_XDC?=${CURRENT_DIR}/MagicLeda.xdc
+LEDA_XDC?=${THIS_DIR}/MagicLeda.xdc
+LEDA_SPINAL?=${THIS_DIR}/MagicLeda.scala
 LEDA_PLATFORM?=xc7a35tcsg324-1
 LEDA_FLASH?=n25q64-3.3v-spi-x1_x2_x4
