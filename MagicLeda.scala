fae43189ac941f2f9eecf1c3267f9dd8739cc436
liyuxuan <liyuxuan3003@outlook.com>
2026-02-05 15:47:02 +0800

    尝试将key switch led都改成inout模式

--- MagicLeda.scala
+++ MagicLeda.scala
@@ -4,14 +4,13 @@ import spinal.core._
 import spinal.lib._
 
 case class MagicLeda() extends Bundle with IMasterSlave {
-  val key = Bits(4 bits)
-  val switch = Vec(Bits(8 bits), 2)
-  val led = Vec(Bits(8 bits), 2)
+  val key = Analog(Bits(4 bits))
+  val switch = Analog(Vec(Bits(8 bits), 2))
+  val led = Analog(Vec(Bits(8 bits), 2))
 
-  led.default(Vec(B(0, 8 bits), 2))
+  // led.default(Vec(B(0, 8 bits), 2))
 
   override def asMaster(): Unit = {
-    in(key, switch)
-    out(led)
+    inout(led, switch, key)
   }
 }
