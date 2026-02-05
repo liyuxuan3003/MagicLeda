3ff77f2437cbb204565db572e3f5742794cf7b31
liyuxuan <liyuxuan3003@outlook.com>
2026-02-05 17:27:31 +0800

    改用TriState

--- MagicLeda.scala
+++ MagicLeda.scala
@@ -2,15 +2,14 @@ package magicleda
 
 import spinal.core._
 import spinal.lib._
+import spinal.lib.io.TriState
 
 case class MagicLeda() extends Bundle with IMasterSlave {
-  val key = Analog(Bits(4 bits))
-  val switch = Analog(Vec(Bits(8 bits), 2))
-  val led = Analog(Vec(Bits(8 bits), 2))
+  val key = Vec(master(TriState(Bool())), 8)
+  val switch = Vec(Vec(master(TriState(Bool())), 8), 2)
+  val led = Vec(Vec(master(TriState(Bool())), 8), 2)
 
   // led.default(Vec(B(0, 8 bits), 2))
 
-  override def asMaster(): Unit = {
-    inout(led, switch, key)
-  }
+  override def asMaster(): Unit = {}
 }
