7f8d90debc7adf0f1632c9d5f86f5718759c9856
liyuxuan <liyuxuan3003@outlook.com>
2026-02-05 22:10:12 +0800

    再次回到简单的in/out

--- MagicLeda.scala
+++ MagicLeda.scala
@@ -5,25 +5,16 @@ import spinal.lib._
 import spinal.lib.io.TriState
 
 case class MagicLeda() extends Bundle with IMasterSlave {
-  val key = Vec(TriState(Bool()), 8)
-  val switch = Vec(Vec(TriState(Bool()), 8), 2)
-  val led = Vec(Vec(TriState(Bool()), 8), 2)
+  val key = Bits(4 bits)
+  val switch = Vec(Bits(8 bits), 2)
+  val led = Vec(Bits(8 bits), 2)
 
-  def defaultTri(t: TriState[Bool]): Unit = {
-    t.read.default(False)
-    t.write.default(False)
-    t.writeEnable.default(False)
-  }
-  def defaultTriVec1(trivec1: Vec[TriState[Bool]]): Unit = trivec1.foreach { defaultTri(_) }
-  def defaultTriVec2(trivec2: Vec[Vec[TriState[Bool]]]): Unit = trivec2.foreach { defaultTriVec1(_) }
-
-  defaultTriVec1(key)
-  defaultTriVec2(switch)
-  defaultTriVec2(led)
+  key.default(B(0, 4 bits))
+  switch.default(Vec(B(0, 8 bits), 2))
+  led.default(Vec(B(0, 8 bits), 2))
 
   override def asMaster(): Unit = {
-    key.foreach { t => master(t) }
-    switch.foreach { p => p.foreach { t => master(t) } }
-    led.foreach { p => p.foreach { t => master(t) } }
+    in(key, switch)
+    out(led)
   }
 }
