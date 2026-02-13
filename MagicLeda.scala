4f68ac1e09add637133950e599e15742028dd172
liyuxuan <liyuxuan3003@outlook.com>
2026-02-13 21:28:32 +0800

    增加对digit的支持

--- MagicLeda.scala
+++ MagicLeda.scala
@@ -8,13 +8,22 @@ case class MagicLeda() extends Bundle with IMasterSlave {
   val key = Bits(4 bits)
   val switch = Vec(Bits(8 bits), 2)
   val led = Vec(Bits(8 bits), 2)
+  val digit = Vec(DigitPin(), 2)
 
   key.default(B(0, 4 bits))
   switch.default(Vec(B(0, 8 bits), 2))
   led.default(Vec(B(0, 8 bits), 2))
+  digit.default(Vec(DigitPin(), 2))
 
   override def asMaster(): Unit = {
     in(key, switch)
-    out(led)
+    out(led, digit)
   }
+}
+
+case class DigitPin() extends Bundle {
+  val seg = Bits(8 bits)
+  val sel = Bits(4 bits)
+  seg.default(B(0, 8 bits))
+  sel.default(B(0, 4 bits))
 }
